b796ca9354e484e24184b698f09fe45668c874da
yanthink <yanthink911@qq.com>
2020-05-27 20:37:50 +0800

    添加百度统计代码

--- src/global.tsx
+++ src/global.tsx
@@ -9,3 +9,12 @@ moment.locale('zh-en');
 emojiToolkit.sprites = true;
 emojiToolkit.spriteSize = 32;
 resetMarkedOptions();
+
+if (window.location.hostname === 'www.einsition.com') {
+  // 百度统计
+  (() => {
+    const hm = document.createElement('script');
+    hm.src = 'https://hm.baidu.com/hm.js?ac1bc08008f195f8b3c753b4b718104b';
+    document.head.appendChild(hm);
+  })();
+}
